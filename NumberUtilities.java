d5fbcd4463a84e030340a41373ff5b870433557d
Henry <cataniahf@gmail.com>
2019-05-25 12:46:39 -0400

    completed lab

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -2,31 +2,58 @@
 
 
 public class NumberUtilities {
+        public static String getRange(int stop) {
+            String numString = "";
+            for(int x = 0; x < stop; x++){
+                numString += x;
+            }
+            return numString;
+        }
 
-    public static String getRange(int stop) {
-        return null;
-    }
-
-    public static String getRange(int start, int stop) {
-        return null;
-    }
-
-
-    public static String getRange(int start, int stop, int step) {
-        return null;
-    }
-
-    public static String getEvenNumbers(int start, int stop) {
-        return null;
-    }
+        public static String getRange(int start, int stop) {
+            String numString = "";
+            for(int x = start; x < stop; x++){
+                numString += x;
+            }
+            return numString;
+        }
 
 
-    public static String getOddNumbers(int start, int stop) {
-        return null;
-    }
+        public static String getRange(int start, int stop, int step) {
+            String numString = "";
+            for(int x = start; x < stop; x =+ step){
+                numString += x;
+            }
+            return numString;
+        }
+
+        public static String getEvenNumbers(int start, int stop) {
+            String numString = "";
+            for (int x = start; x < stop; x++) {
+                if (x % 2 == 0) {
+                    numString += x;
+                }
+            }
+            return numString;
+        }
 
 
-    public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
-    }
-}
+            public static String getOddNumbers(int start, int stop) {
+                String numString = "";
+                for(int x = start; x < stop; x++){
+                    if(x % 2 ==1){
+                        numString += x;
+                    }
+                }
+                return numString;
+            }
+
+
+            public static String getExponentiations(int start, int stop, int exponent) {
+                String numString = "";
+                for(int x = start; x <= stop; x++){
+                    numString += ((int)Math.pow(x, exponent));
+                }
+                return numString;
+            }
+        }
--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,67 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String result = "";
+        String rowString = "";
+        for(int i = 1; i < 6; i++){
+            for(int j = 1; j < 6; j++){
+                int numHolder = i * j;
+                if (numHolder < 10){
+                    rowString += "  " + numHolder + " |";
+                } else if (numHolder < 100){
+                    rowString += " " + numHolder + " |";
+                } else {
+                    rowString += numHolder + " |";
+                }
+            }
+         result += rowString + "\n";
+         rowString = "";
+        }
+        System.out.println(result);
+        return result;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String result = "";
+        String rowString = "";
+        for(int i = 1; i < 11; i++){
+            for(int j = 1; j < 11; j++){
+                int numHolder = i * j;
+                if (numHolder < 10){
+                    rowString += "  " + numHolder + " |";
+                } else if (numHolder < 100){
+                    rowString += " " + numHolder + " |";
+                } else if (numHolder >100){
+                    rowString += "" + numHolder + " |";
+                } else {
+                    rowString += numHolder + " |";
+                }
+            }
+         result += rowString + "\n";
+         rowString = "";
+        }
+        System.out.println(result);
+        return result;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+        String rowString = "";
+        for(int i = 1; i <= tableSize; i++){
+            for(int j = 1; j <= tableSize; j++){
+                int numHolder = i * j;
+                if (numHolder < 10){
+                    rowString += "  " + numHolder + " |";
+                } else if (numHolder < 100){
+                    rowString += " " + numHolder + " |";
+                } else {
+                    rowString += numHolder + " |";
+                }
+            }
+         result += rowString + "\n";
+         rowString = "";
+        }
+        System.out.println(result);
+        return result;
     }
 }
